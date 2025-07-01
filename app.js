86d47f5033925aecee31a261771cd612ee5959f4
Tamanna Mondal <info@intelbyte.in>
2025-07-01 17:38:28 +0530

    Fix routing and static assets, add page routing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
 
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
