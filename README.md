84d0aad524ec78f1ec4ecc16373ea4e4aefd9a5d
Tamanna Mondal <info@intelbyte.in>
2025-07-01 16:49:56 +0530

    add project files

---
+++ README.md
@@ -0,0 +1,2 @@
+# intelbytewebsite
+# infointelbyte
---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+ 
+const methodOverride = require('method-override') ;
+
+const flash = require('connect-flash');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const nodemailer = require('nodemailer')
+
+  // App Configuration
+app.use(express.static('aset'));
+app.engine('ejs', ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+// Routing 
+app.get('/intelbyte', (req, res) => {
+   res.render("index.ejs");
+});
+
+app.get('/intelbyte/:page', (req,res) =>{
+  const page = req.params.page;
+
+  const allPage = ['about' , 'package' , 'contact' ,'career' ,  'portfolio' , 'blog' , 'contact' , 'package', 'App Developmentl' , 'Web Development' , 'Software Development' , 'Digital Marketing' , 'Graphic Designing' , 'Blockchain Developmente' ,'Blockchain Development', 'Game Development' ];
+  console.log(page);
+
+  if(allPage.includes(page)){
+    res.render(`${page}.ejs`)
+  }else{
+    res.status(404).send('Page not found ! ')
+  };
+  
+})
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}/intelbyte`);
+});
